c2a680dd8c595de13cfe95b68d9153ed3eef9c28
Melissa Vela <melissa.vela.coral@gmail.com>
2024-09-03 20:34:03 +1000

    Stripe integration

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ import userRouter from "./routers/userRouter.js";
 import categoryRouter from "./routers/categoryRouter.js";
 import productRouter from "./routers/productRouter.js";
 import addressRouter from "./routers/addressRouter.js";
+import Stripe from "stripe";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,26 @@ app.use("/api/user", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
 app.use("/api/address", addressRouter);
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    console.log("entered create-payment-intent ");
+    const { amount, currency, customer } = req.body;
+    console.log(amount);
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: currency,
+      amount: amount || 10,
+      metadata: { customerName: customer },
+      automatic_payment_methods: { enabled: true },
+    });
+    res.json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
 
 // Start Server
 app.listen(PORT, (error) => {
