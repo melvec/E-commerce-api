42859004a87a189d1f44902809b789a17bcdd627
Melissa Vela <melissa.vela.coral@gmail.com>
2025-08-02 12:15:31 +1000

    mongo db connection

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,31 @@
+// import mongoose from "mongoose";
+
+// export const connectToMongoDb = () => {
+//   try {
+//     const connect = mongoose.connect(
+//       process.env.DB_CONNECT_URL + "/ecommerce-db"
+//     );
+//     if (connect) {
+//       console.log(
+//         `Database conected: ${process.env.DB_CONNECT_URL}/ecommerce-db`
+//       );
+//     }
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+// };
+
 import mongoose from "mongoose";
 
-export const connectToMongoDb = () => {
+export const connectToMongoDb = async () => {
   try {
-    const connect = mongoose.connect(
-      process.env.DB_CONNECT_URL + "/ecommerce-db"
-    );
-    if (connect) {
-      console.log(
-        `Database conected: ${process.env.DB_CONNECT_URL}/ecommerce-db`
-      );
-    }
+    await mongoose.connect(process.env.DB_CONNECT_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
-    console.log("Error:", error);
+    console.error("❌ MongoDB connection error:", error.message);
   }
 };
